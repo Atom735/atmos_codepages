f21547c0c51b01464061618101f4ca468d50e48c
Atom735 <Atom735.ag47@gmail.com>
2022-02-21 16:20:09 +0300

    first commit

---
+++ example/atmos_codepages_example.dart
@@ -0,0 +1,6 @@
+import 'package:atmos_codepages/atmos_codepages.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/atmos_codepages_test.dart
@@ -0,0 +1,16 @@
+import 'package:atmos_codepages/atmos_codepages.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
